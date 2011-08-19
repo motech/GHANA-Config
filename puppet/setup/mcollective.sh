54a9babb0447c9e7feccd6282c881bb0893e3ac2
Puneet Goyal <pungoyal@gmail.com>
2011-08-19 15:08:14 +0530

    moving files to subduers

--- puppet/setup/mcollective.sh
+++ puppet/setup/mcollective.sh
@@ -1,18 +1,14 @@
-# node11
-
 sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/puppet.repo -O /etc/yum.repos.d/puppet.repo
-sudo rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs
-sudo yum -y upgrade
+# sudo yum -y upgrade
 
-sudo yum install -y java-1.6.0-openjdk activemq
-sudo /sbin/service activemq start
+sudo yum install -y java-1.6.0-openjdk 
+sudo rpm -Uvh http://downloads.puppetlabs.com/mcollective/tanukiwrapper-3.2.3-1jpp.x86_64.rpm
+sudo yum install -y  activemq
+sudo /sbin/service activemq restart
 
 sudo yum install -y mcollective mcollective-client mcollective-common rubygem-stomp
 
 # change stomp host and plugin.psk in /etc/mcollective/client.cfg and /etc/mcollective/server.cfg
-
-sudo /sbin/service mcollective stop
 sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/client.cfg -O /etc/mcollective/client.cfg
 sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/server.cfg -O /etc/mcollective/server.cfg
-sudo /sbin/service mcollective start
-
+sudo /sbin/service mcollective restart
