71a403b9d3a9caf1257278d968c30b7fe449f7c4
Puneet Goyal <pungoyal@gmail.com>
2011-08-21 12:30:15 +0530

    fixing ip addresses

--- puppet/setup/mcollective.sh
+++ puppet/setup/mcollective.sh
@@ -1,7 +1,6 @@
 sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/setup/puppetlabs.repo -O /etc/yum.repos.d/puppetlabs.repo
-# sudo yum -y upgrade
 
-sudo yum install -y java-1.6.0-openjdk 
+sudo yum install -y java-1.6.0-openjdk
 sudo rpm -Uvh http://downloads.puppetlabs.com/mcollective/tanukiwrapper-3.2.3-1jpp.x86_64.rpm
 sudo yum install -y  activemq
 sudo /sbin/service activemq restart
@@ -9,6 +8,6 @@ sudo /sbin/service activemq restart
 sudo yum install -y mcollective mcollective-client mcollective-common rubygem-stomp
 
 # change stomp host and plugin.psk in /etc/mcollective/client.cfg and /etc/mcollective/server.cfg
-sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/client.cfg -O /etc/mcollective/client.cfg
-sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/server.cfg -O /etc/mcollective/server.cfg
+sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/setup/client.cfg -O /etc/mcollective/client.cfg
+sudo wget https://raw.github.com/motech/GHANA-Config/master/puppet/setup/server.cfg -O /etc/mcollective/server.cfg
 sudo /sbin/service mcollective restart
