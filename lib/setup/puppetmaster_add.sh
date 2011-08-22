0877c0f136697fef0e74c35f209d6bb04fbd0066
Puneet Goyal <pungoyal@gmail.com>
2011-08-22 15:58:34 +0530

    puppet top level restructure

--- lib/setup/puppetmaster_add.sh
+++ lib/setup/puppetmaster_add.sh
@@ -3,6 +3,34 @@
 rpm -Uvh http://yum.puppetlabs.com/base/puppetlabs-repo-3.0-2.noarch.rpm
 rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 
-yum install -y ruby rubygems
+yum install -y ruby rubygems git
 yum --enablerepo=epel-testing,tw_devops_custom install -y puppet-server
+
+collective="noop"
+repo_location="/usr/share/puppet-recipes/$collective"
+
+if [ -d $repo_location ] ; then
+  rm -rf $repo_location
+fi
+
+mkdir -p $repo_location
+git clone git://github.com/motech/GHANA-Config.git $repo_location
+
+cat > /etc/puppet/puppet.conf <<EOF
+[main]
+    vardir = /var/lib/puppet
+    logdir = /var/log/puppet
+    rundir = /var/run/puppet
+    ssldir = $vardir/ssl
+
+[$collective]
+    manifest = $repo_location/GHANA-Config/puppet/manifests/site.pp
+    modulepath = $repo_location/GHANA-Config/puppet/modules
+EOF
+
+cat > /etc/puppet/fileserver.conf <<EOF
+[modules]
+  allow *
+EOF
+
 /sbin/service puppetmaster start
---
+++ puppet/manifests/site.pp
@@ -0,0 +1,6 @@
+import 'nodes/nodes'
+
+Exec {
+  path => "/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin"
+}
+
