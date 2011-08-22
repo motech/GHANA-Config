22816a6bd795481f7ab8586029a50a81e6d5f630
Puneet Goyal <pungoyal@gmail.com>
2011-08-22 15:58:55 +0530

    setting up nodes

--- puppet/manifests/nodes/nodes.pp
+++ puppet/manifests/nodes/nodes.pp
@@ -1,18 +1,28 @@
 node base {
+  package { 'htop': ensure => installed }
   include mcollective::agent
 }
 
 node default inherits base {
 }
 
-node 'sifynode42.thoughtworks.com' inherits base {
-  include activemq
-  include puppetmaster
-}
-
-node 'sifynode35.thoughtworks.com' inherits base {
+node 'sifynode36.thoughtworks.com' inherits base {
   include jenkins
 }
 
-node 'sifynode36.thoughtworks.com' inherits base {
+node 'sifynode37.thoughtworks.com' inherits base {
+}
+
+node 'sifynode41.thoughtworks.com' inherits base {
+}
+
+node 'sifynode42.thoughtworks.com' inherits base {
+  include puppetmaster
+  include activemq
+}
+
+node 'sifynode43.thoughtworks.com' inherits base {
+}
+
+node 'sifynode44.thoughtworks.com' inherits base {
 }
