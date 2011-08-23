cce922bc1a3cbabaf635672758f3eb1b70f3c77b
Puneet Goyal <pungoyal@gmail.com>
2011-08-23 14:28:26 +0530

    making sure file is there to override

--- puppet/modules/mcollective/manifests/client.pp
+++ puppet/modules/mcollective/manifests/client.pp
@@ -5,6 +5,12 @@ class mcollective::client inherits mcollective {
     require  => Package["mcollective-common"]
   }
 
+  file { "/etc/mcollective/client.cfg":
+    ensure  => present,
+    mode    => 640,
+    require => Package["mcollective-client"]
+  }
+
   File["/etc/mcollective/client.cfg"] {
     content => template("mcollective/client.cfg.erb")
   }
