c12e87e7c9aa6f34b753875ea5d446be09f62861
Puneet Goyal <pungoyal@gmail.com>
2011-08-23 13:10:07 +0530

    installing java as a dependency for jenkins

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -1,9 +1,10 @@
 class jenkins {
   include repos::jenkins
+  include java::open_jdk
 
   package { "jenkins":
     ensure  =>  "present",
-    require   => Yumrepo[jenkins]
+    require   => Yumrepo[jenkins], Package["java-1.6.0-openjdk"]
   }
 
   service {"jenkins":
