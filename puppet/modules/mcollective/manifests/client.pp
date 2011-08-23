1790c586ccc5ff376956222b6cbf19975c8e930a
Puneet Goyal <pungoyal@gmail.com>
2011-08-23 12:49:34 +0530

    do not need to be root for client config

--- puppet/modules/mcollective/manifests/client.pp
+++ puppet/modules/mcollective/manifests/client.pp
@@ -5,14 +5,6 @@ class mcollective::client inherits mcollective {
     require  => Package["mcollective-common"]
   }
 
-  file { "/etc/mcollective/client.cfg":
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 640,
-    require => Package["mcollective-client"]
-  }
-
   File["/etc/mcollective/client.cfg"] {
     content => template("mcollective/client.cfg.erb")
   }
