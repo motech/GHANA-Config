a52cd1d9cb96b51801aab6211865b52203823188
Puneet Goyal <pungoyal@gmail.com>
2011-08-23 10:23:06 +0530

    fixing tanukiwrapper install

--- puppet/modules/activemq/manifests/init.pp
+++ puppet/modules/activemq/manifests/init.pp
@@ -2,11 +2,7 @@ class activemq {
   include repos::puppetlabs
   include ruby::rubygem_stomp
   include java::open_jdk
-
-  package { "tanukiwrapper":
-    ensure  =>  "present",
-    require => Package[puppetlabs-repo]
-  }
+  include tanukiwrapper
 
   package { "activemq":
     ensure    => "present",
--- puppet/modules/repos/manifests/epel.pp
+++ puppet/modules/repos/manifests/epel.pp
@@ -2,6 +2,7 @@ class repos::epel {
   file { "/usr/local/src/epel-release-5-4.noarch.rpm":
     source => "puppet://puppet/modules/repos/epel-release-5-4.noarch.rpm"
   }
+
   package { "epel-release-5-4":
     provider => "rpm",
     ensure => "present",
