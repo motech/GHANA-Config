be580b705f78b2f4b860f7ae78b6eb87f897cdde
Puneet Goyal <pungoyal@gmail.com>
2011-08-23 15:23:01 +0530

    git for jenkins machine

--- puppet/manifests/nodes/ci.pp
+++ puppet/manifests/nodes/ci.pp
@@ -2,6 +2,7 @@ node ci inherits base {
   $collective = "ci"
 }
 
-node 'sifynode37.thoughtworks.com' inherits base {
+node 'sifynode37.thoughtworks.com' inherits ci {
+  include git
   include jenkins
 }
--- puppet/manifests/nodes/dev.pp
+++ puppet/manifests/nodes/dev.pp
@@ -1,4 +1,8 @@
-node 'sifynode42.thoughtworks.com' inherits base {
+node ci inherits base {
+  $collective = "dev"
+}
+
+node 'sifynode42.thoughtworks.com' inherits dev {
   # include puppetmaster
   include activemq
   include mcollective::client
