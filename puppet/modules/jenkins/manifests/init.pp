fd76098ea8a7b45e987c8dacea7779ccaa78b656
Puneet Goyal <pungoyal@gmail.com>
2011-08-23 13:06:32 +0530

    removing gig check for jenkins

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -5,4 +5,11 @@ class jenkins {
     ensure  =>  "present",
     require   => Yumrepo[jenkins]
   }
+
+  service {"jenkins":
+      enable  => true,
+      ensure  => "running",
+      hasrestart=> true,
+      require => Package["jenkins"],
+  }
 }
--- puppet/modules/repos/manifests/jenkins.pp
+++ puppet/modules/repos/manifests/jenkins.pp
@@ -3,7 +3,7 @@ class repos::jenkins {
     descr    => 'Repo for jenkin',
     baseurl  => 'http://pkg.jenkins-ci.org/redhat/',
     enabled  => 1,
-    gpgcheck => 1,
+    gpgcheck => 0,
     gpgkey => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
   }
 }
