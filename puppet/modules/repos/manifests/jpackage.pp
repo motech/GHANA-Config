4dc8503c0141e20eff0546cfe85dea7e7d6c89f5
Puneet Goyal <pungoyal@gmail.com>
2011-09-08 11:49:33 +0530

    pgoyal | fixing jpackage repo

--- puppet/modules/repos/manifests/jpackage.pp
+++ puppet/modules/repos/manifests/jpackage.pp
@@ -1,12 +1,7 @@
 class repos::jpackage {
-  file { "/usr/local/src/jpackage-utils-1.7.5-1jpp.noarch.rpm":
-    source => "puppet://puppet/modules/repos/jpackage-utils-1.7.5-1jpp.noarch.rpm"
-  }
-
-  package { "jpackage-utils":
-    provider => "rpm",
-    ensure => "present",
-    source => "/usr/local/src/jpackage-utils-1.7.5-1jpp.noarch.rpm",
-    require => File["/usr/local/src/jpackage-utils-1.7.5-1jpp.noarch.rpm"]
+  file { "jpackage50":
+    name => "/etc/yum.repos.d/jpackage50.repo",
+    ensure => present,
+    source => "puppet://puppet/modules/repos/jpackage50.repo"
   }
 }
